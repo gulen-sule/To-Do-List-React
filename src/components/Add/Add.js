8ca37da10d53e7abc3a4735c5313616e663dd448
gulen-sule <gulensule@gmail.com>
2022-01-18 15:25:15 +0300

    desinged the page commit

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -5,33 +5,115 @@ import ToDoList from "../ToDo/ToDoList"
 function Form(){
 
     const [items, setItem] = useState(["temizlik","ödev"]);
-    
+    const [editable, setEditable] = useState([false,false]);
+    const [hasChosen,setHasChosen]=useState(false);
+    const [chosenIndexes,setChosenIndexes]=useState([]);
+   
+
     const handleClick = (event) => {
-        event.preventDefault();
+        if(hasChosen){
+            for (let i in hasChosen)
+            {
+               // removeItem(i)
+                console.log(i," silindi")
+            }
+        }
+        else{
+           event.preventDefault();
         //window.alert(event.target[0].value)
         if(event.target[0].value!=="")
         setItem([...items,event.target[0].value])
-        event.target[0].value=""
+        event.target[0].value="" 
+        }
       }
 
+    function editList(event,index){
+        items[index]=event.target.value
+        console.log("here")
+        setItem([...items])
+    }
     function removeItem(index){
         items.splice(index,1)
         setItem([...items])
     }
+    function setOpp(index){
+        editable[index]=!editable[index]
+        setEditable([...editable])
+    }
+    
+    function checked(index) {
+        for (let a in chosenIndexes)
+        console.log(a)
 
+        console.log("break",index)
+
+        if(!chosenIndexes.includes(index,0)){
+            setHasChosen(true)
+            setChosenIndexes([...chosenIndexes,index])
+           // console.log(chosenIndexes.length)
+        }
+        else{
+            chosenIndexes.splice(chosenIndexes.indexOf(index),1)
+            if(chosenIndexes.length===0)
+            {setHasChosen(false)}
+        }
+        
+        
+    }
+
+    function editItem(index){
+        return (
+        <div className="row">
+             <div className="col-11">
+                 <input type='text' value={items[index]} 
+                 onChange={(e)=>editList(e,index)} className="input-box" />
+             </div>
+             <div className="col-1">
+                 <button className="btn-edit" type="button" 
+                 onClick={()=>setOpp(index)}>+</button>
+             </div>
+         </div>)
+ 
+     }
+
+    function listItem(index){
+        return(
+        <>  
+            <div  className="col-1">
+            <input type="checkbox" name={index} onClick={(e)=>checked(index)}></input>
+            </div>
+            <div  className="col-9">
+                <ToDoList item={items[index]} ></ToDoList>
+            </div>
+            <div className="col-1">
+                <button className="btn_edit"  type="button" 
+                onClick={()=>setOpp(index)}>E</button>
+            </div>
+            <div className="col-1">
+                <button className="btn_remove" type="button" 
+                onClick={()=>removeItem(index)}>x</button>
+            </div>
+        </>
+        )
+    }
+
+    
     return(
     <div className="container">
         <div className="row">
-            <form  onSubmit={handleClick}>
+            <form  onSubmit={handleClick} >
             <div className="container">
                 <div className="row">
                     <div className="col-sm-10">
                     <label  className="input" >
-                        <input type="text" id="to-do-input" />
+                        <input type="text" id="to-do-input" className="input-box" />
                     </label>
                     </div>
                     <div  className="col-sm-2">
-                    <button className="btn-add" type="Submit">Add</button>
+                    {hasChosen ? 
+                    <button className="btn-add" type="Submit">Delete</button>:
+                    <button className="btn-add" type="Submit">Add</button>}
+                    
                     </div>
                 </div>
             </div>
@@ -43,15 +125,9 @@ function Form(){
             {items.map((d,index)=>
             <div className="col-12" key={index}>
                 <li className="row row_list">
-                    <div className="col-2" >
-                        <input type="checkbox"  id="check1" />
-                    </div>
-                    <div  className="col-9">
-                        <ToDoList item={d} ></ToDoList>
-                    </div>
-                    <div className="col-1">
-                        <button className="btn_remove" id={index}  type="button" onClick={()=>removeItem(index)}>x</button>
-                    </div>
+            {  !editable[index] ?
+                ( listItem(index)):
+                (  editItem(index))}
                 </li>
             </div>
             )}
